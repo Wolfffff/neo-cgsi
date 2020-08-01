96d084a07f72adbfe165783ac45c6883bec3fe87
wolf <wolf@wolfs-MacBook-Pro.local>
2020-08-01 16:53:48 -0400

    adding first set of plots -- nearest TSS for 3 samples individually and as a group

--- 20200719_AnalysisStart.R
+++ 20200719_AnalysisStart.R
@@ -1,17 +1,58 @@
 library(purrr)
 library(tidyverse)
 library(edgeR)
-f_files<- list.files("neo-cgsi/macs2/S4", pattern = "nar", full.names = T) #List of files to concatenate
-read_in_feature_counts<- function(file){
-  cnt<- read_tsv(file, col_names = F, comment = "#")
-  colnames(cnt) <- c("chr", "startCoord", "endCoord", "name", "score", "strand", "signalValue", "pValue", "qValue", "peak")
-  cnt<- cnt %>% dplyr::select(1, 2, 3)
-  return(cnt) #Function to concat the files, but only adds the Chr, Start, End, Str, Length from the first
+library(tools)
+
+curr_time <- function(){
+  return(format(Sys.time(), "%d%m%Y_%H%M"))
 }
-TM3Seq_counts<- map(f_files, read_in_feature_counts) #Does the function above for the list given
-TM3Seq_counts_df<- purrr::reduce(TM3Seq_counts, inner_join) #Makes into a data frame 
-BiocManager::install("org.Hs.eg.db")
-nearestTSS("chr5", 385724, species="Hs")
-TM3Seq_counts_df$nearTSS <- nearestTSS(TM3Seq_counts_df$chr, 0.5*(TM3Seq_counts_df$startCoord + TM3Seq_counts_df$endCoord), species="Hs")
-TM3Seq_counts_df <- TM3Seq_counts_df[TM3Seq_counts_df$nearTSS$distance > -10000 & TM3Seq_counts_df$nearTSS$distance < 10000,]
-p <- ggplot(TM3Seq_counts_df, aes(x=nearTSS$distance)) + geom_histogram(bins=1000)
+
+plots_dir <- "results/plots/"
+narrowPeak_cols <- c("chr", "startCoord", "endCoord", "name", "score", "strand", "signalValue", "pValue", "qValue", "peak")
+
+files <- list.files(pattern = "nar", full.names = T,recursive=T) #List of files to concatenate
+
+samples = list()
+
+for (file in files) {
+  temp_read <- read_tsv(file,col_names = F)
+  colnames(temp_read) <- narrowPeak_cols
+  temp_read$nearTSS <- nearestTSS(temp_read$chr, 0.5*(temp_read$startCoord + temp_read$endCoord), species="Hs")
+  samples[[file_path_sans_ext(basename(file))]]  <- temp_read
+}
+
+for (name in names(samples)){
+  sample = samples[[name]]
+  print(ggplot(sample,aes(x=nearTSS$distance)) +
+    geom_histogram(aes(y=..density..),binwidth=1000,col="darkblue") +
+    xlim(-1e5,1e5) +
+    xlab("Distance") +
+    ylab("Count") + 
+    ggtitle(paste0("Nearest TSS Distribution - ",name," - ",curr_time())) +
+    theme_minimal())
+  ggsave(file=paste0(plots_dir,"NearestTSS_",name,"_",curr_time(),".png"))
+}
+ggplot(samples[[1]],aes(x=nearTSS$distance)) +
+          geom_histogram(aes(y=..density..,fill="red"),binwidth=2000,alpha=.4) +
+          geom_histogram(data=samples[[2]],aes(nearTSS$distance,y=..density..,,fill="green"),binwidth=2000,alpha=.4) +
+          geom_histogram(data=samples[[3]],aes(x=nearTSS$distance,y=..density..,fill="blue"),binwidth=2000,alpha=.4) +
+          xlim(-1e5,1e5) +
+          xlab("Distance") +
+          ylab("Density") +
+          ggtitle("Nearest TSS Distribution") +
+          scale_fill_discrete(name = "Sample",labels=c(names(samples))) +
+  theme_minimal()
+ggsave(file=paste0(plots_dir,"NearestTSS_GroupedPlot_",curr_time(),".png"))
+
+ggplot(samples[[1]],aes(x=nearTSS$distance)) +
+  geom_density(aes(y=..density..,col="red")) +
+  geom_density(data=samples[[2]],aes(nearTSS$distance,y=..density..,,col="green")) +
+  geom_density(data=samples[[3]],aes(x=nearTSS$distance,y=..density..,col="blue")) +
+  xlim(-1e5,1e5) +
+  xlab("Distance") +
+  ylab("Density") +
+  ggtitle("Nearest TSS Distribution") +
+  scale_color_discrete(name = "Sample",labels=c(names(samples))) +
+  scale_fill_manual(values = c("red","green","blue"), name = "text",labels=c(names(samples))) +
+  theme_minimal() 
+ggsave(file=paste0(plots_dir,"NearestTSS_GroupedPlot_KDE_",curr_time(),".png"))
