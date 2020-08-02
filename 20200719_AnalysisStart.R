2c4152956d03ac71219a3300d66399734c04e0f3
wolf <wolf@wolfs-MacBook-Pro.local>
2020-08-01 21:03:56 -0400

    ChIPseeker

--- 20200719_AnalysisStart.R
+++ 20200719_AnalysisStart.R
@@ -2,6 +2,9 @@ library(purrr)
 library(tidyverse)
 library(edgeR)
 library(tools)
+library(ChIPseeker)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 
 curr_time <- function(){
   return(format(Sys.time(), "%d%m%Y_%H%M"))
@@ -17,37 +20,39 @@ samples = list()
 for (file in files) {
   temp_read <- read_tsv(file,col_names = F)
   colnames(temp_read) <- narrowPeak_cols
-  temp_read$nearTSS <- nearestTSS(temp_read$chr, 0.5*(temp_read$startCoord + temp_read$endCoord), species="Hs")
+  temp_TSS <- nearestTSS(temp_read$chr, 0.5*(temp_read$startCoord + temp_read$endCoord), species="Hs")
+  colnames(temp_TSS) = paste0("TSS_Nearest.",colnames(temp_TSS))
+  temp_read <- cbind(temp_read,temp_TSS)
   samples[[file_path_sans_ext(basename(file))]]  <- temp_read
 }
 
 for (name in names(samples)){
   sample = samples[[name]]
-  print(ggplot(sample,aes(x=nearTSS$distance)) +
+  print(ggplot(sample,aes(x=TSS_Nearest.distance)) +
     geom_histogram(aes(y=..density..),binwidth=1000,col="darkblue") +
     xlim(-1e5,1e5) +
     xlab("Distance") +
     ylab("Count") + 
     ggtitle(paste0("Nearest TSS Distribution - ",name," - ",curr_time())) +
     theme_minimal())
-  ggsave(file=paste0(plots_dir,"NearestTSS_",name,"_",curr_time(),".png"))
+  # ggsave(file=paste0(plots_dir,"NearestTSS_",name,"_",curr_time(),".png"))
 }
-ggplot(samples[[1]],aes(x=nearTSS$distance)) +
+ggplot(samples[[1]],aes(x=TSS_Nearest.distance)) +
           geom_histogram(aes(y=..density..,fill="red"),binwidth=2000,alpha=.4) +
-          geom_histogram(data=samples[[2]],aes(nearTSS$distance,y=..density..,,fill="green"),binwidth=2000,alpha=.4) +
-          geom_histogram(data=samples[[3]],aes(x=nearTSS$distance,y=..density..,fill="blue"),binwidth=2000,alpha=.4) +
+          geom_histogram(data=samples[[2]],aes(TSS_Nearest.distance,y=..density..,,fill="green"),binwidth=2000,alpha=.4) +
+          geom_histogram(data=samples[[3]],aes(TSS_Nearest.distance,y=..density..,fill="blue"),binwidth=2000,alpha=.4) +
           xlim(-1e5,1e5) +
           xlab("Distance") +
           ylab("Density") +
           ggtitle("Nearest TSS Distribution") +
           scale_fill_discrete(name = "Sample",labels=c(names(samples))) +
-  theme_minimal()
-ggsave(file=paste0(plots_dir,"NearestTSS_GroupedPlot_",curr_time(),".png"))
+          theme_minimal()
+# ggsave(file=paste0(plots_dir,"NearestTSS_GroupedPlot_",curr_time(),".png"))
 
-ggplot(samples[[1]],aes(x=nearTSS$distance)) +
+ggplot(samples[[1]],aes(x=TSS_Nearest.distance)) +
   geom_density(aes(y=..density..,col="red")) +
-  geom_density(data=samples[[2]],aes(nearTSS$distance,y=..density..,,col="green")) +
-  geom_density(data=samples[[3]],aes(x=nearTSS$distance,y=..density..,col="blue")) +
+  geom_density(data=samples[[2]],aes(TSS_Nearest.distance,y=..density..,,col="green")) +
+  geom_density(data=samples[[3]],aes(x=TSS_Nearest.distance,y=..density..,col="blue")) +
   xlim(-1e5,1e5) +
   xlab("Distance") +
   ylab("Density") +
@@ -55,4 +60,22 @@ ggplot(samples[[1]],aes(x=nearTSS$distance)) +
   scale_color_discrete(name = "Sample",labels=c(names(samples))) +
   scale_fill_manual(values = c("red","green","blue"), name = "text",labels=c(names(samples))) +
   theme_minimal() 
-ggsave(file=paste0(plots_dir,"NearestTSS_GroupedPlot_KDE_",curr_time(),".png"))
+# ggsave(file=paste0(plots_dir,"NearestTSS_GroupedPlot_KDE_",curr_time(),".png"))
+
+
+
+
+peaks = readPeakFile(files[[2]])
+# covplot(peaks, weightCol="V5")
+
+promoter <- getPromoters(TxDb=txdb, upstream=3000, downstream=3000)
+tagMatrix <- getTagMatrix(peaks, windows=promoter)
+tagHeatmap(tagMatrix, xlim=c(-3000, 3000), color="red")
+
+plotAvgProf(tagMatrix, xlim=c(-3000, 3000),xlab="Genomic Region (5'->3')", ylab = "Read Count Frequency")
+plotAvgProf(tagMatrix, xlim=c(-3000, 3000), conf = 0.95, resample = 1000)
+peakAnno <- annotatePeak(files[[1]], tssRegion=c(-3000, 3000),TxDb=txdb, annoDb="org.Hs.eg.db")
+plotAnnoPie(peakAnno)
+plotAnnoBar(peakAnno)
+vennpie(peakAnno)
+upsetplot(peakAnno)
