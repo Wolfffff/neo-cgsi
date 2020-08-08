b954534f4c953be3bf636620786acce67fe922d9
wolf <wolf@wolfs-MacBook-Pro.local>
2020-08-07 20:32:44 -0400

    csaw

---
+++ csaw.R
@@ -0,0 +1,138 @@
+# Ref:
+# https://bioconductor.org/packages/3.11/workflows/vignettes/chipseqDB/inst/doc/h3k9ac.html
+
+
+BiocManager::install('csaw')
+library(csaw)
+library(Rsamtools)
+library(edgeR)
+library(ChIPpeakAnno)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+
+
+files <- list.files(pattern = ".bam$", full.names = T,recursive=T) #List of files to concatenate
+acdata <- data.frame(Path=files,Name=basename(files),Description = c("S1","S4","S7"),Lipid=c(FALSE,FALSE,TRUE))
+
+diagnostics <- list()
+for (bam in files) {
+  total <- countBam(bam)$records
+  mapped <- countBam(bam, param=ScanBamParam(
+    flag=scanBamFlag(isUnmapped=FALSE)))$records
+  marked <- countBam(bam, param=ScanBamParam(
+    flag=scanBamFlag(isUnmapped=FALSE, isDuplicate=TRUE)))$records
+  diagnostics[[basename(bam)]] <- c(Total=total, Mapped=mapped, Marked=marked)
+}
+diag.stats <- data.frame(do.call(rbind, diagnostics))
+diag.stats$Prop.mapped <- diag.stats$Mapped/diag.stats$Total*100
+diag.stats$Prop.marked <- diag.stats$Marked/diag.stats$Mapped*100
+diag.stats
+
+standard.chr <- paste0("chr", c(1:23, "X", "Y"))
+param <- readParam(minq=20, restrict=standard.chr)
+
+x <- correlateReads(files, param=reform(param, dedup=TRUE))
+frag.len <- maximizeCcf(x)
+frag.len
+
+plot(1:length(x)-1, x, xlab="Delay (bp)", ylab="CCF", type="l")
+abline(v=frag.len, col="red")
+text(x=frag.len, y=min(x), paste(frag.len, "bp"), pos=4, col="red")
+
+
+win.data <- windowCounts(files, param=param, width=105, ext=frag.len)
+win.data
+
+bins <- windowCounts(files, bin=TRUE, width=2000, param=param)
+filter.stat <- filterWindowsGlobal(win.data, bins)
+min.fc <- 3
+keep <- filter.stat$filter > log2(min.fc)
+summary(keep)
+
+hist(filter.stat$back.abundances, main="", breaks=50,
+     xlab="Background abundance (log2-CPM)")
+threshold <- filter.stat$abundances[1] - filter.stat$filter[1] + log2(min.fc)
+abline(v=threshold, col="red")
+
+filtered.data <- win.data[keep,]
+
+
+win.ab <- scaledAverage(filtered.data)
+adjc <- calculateCPM(filtered.data, use.offsets=FALSE)
+logfc <- adjc[,3] - adjc[,2]
+smoothScatter(win.ab, logfc, ylim=c(-6, 6), xlim=c(0, 5),
+              xlab="Average abundance", ylab="Log-fold change")
+
+lfit <- smooth.spline(logfc~win.ab, df=5)
+o <- order(win.ab)
+lines(win.ab[o], fitted(lfit)[o], col="red", lty=2)
+
+celltype <- acdata$Lipid
+
+celltype <- factor(celltype)
+design <- model.matrix(~0+celltype)
+colnames(design) <- levels(celltype)
+design
+
+
+y <- asDGEList(filtered.data)
+str(y)
+
+y <- estimateDisp(y, design)
+summary(y$trended.dispersion)
+
+plotBCV(y)
+
+fit <- glmQLFit(y, design, robust=TRUE)
+plotQLDisp(fit)
+
+summary(fit$df.prior)
+
+plotMDS(norm.adjc, labels=celltype,
+        col=c("red", "blue")[as.integer(celltype)])
+
+contrast <- makeContrasts(proB-matureB, levels=design)
+res <- glmQLFTest(fit, contrast=contrast)
+head(res$table)
+
+merged <- mergeResults(filtered.data, res$table, tol=100, 
+                       merge.args=list(max.width=5000))
+merged$regions
+
+tabcom <- merged$combined
+tabcom
+
+is.sig <- tabcom$FDR <= 0.05
+summary(is.sig)
+
+table(tabcom$direction[is.sig])
+
+tabbest <- merged$best
+tabbest
+
+is.sig.pos <- (tabbest$rep.logFC > 0)[is.sig]
+summary(is.sig.pos)
+
+out.ranges <- merged$regions
+mcols(out.ranges) <- DataFrame(tabcom,
+                               best.pos=mid(ranges(rowRanges(filtered.data[tabbest$rep.test]))),
+                               best.logFC=tabbest$rep.logFC)
+saveRDS(file="brant_results.rds", out.ranges)
+
+simplified <- out.ranges[is.sig]
+simplified$score <- -10*log10(simplified$FDR)
+export(con="brant_results.bed", object=simplified)
+
+
+
+
+# Interpretation
+
+anno <- detailRanges(out.ranges, orgdb=org.Mm.eg.db, txdb=TxDb.Mmusculus.UCSC.mm10.knownGene)
+head(anno$overlap)
+
+head(anno$left)
+head(anno$right)
+
+meta <- mcols(out.ranges)
+mcols(out.ranges) <- data.frame(meta, anno)
